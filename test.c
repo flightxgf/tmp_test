518ee9219061590026bf6bbbf7b2675ee92bd29b
flightxgf <18706775913@163.com>
2018-04-15 03:26:34 -0700

    git index

--- test.c
+++ test.c
@@ -4,7 +4,19 @@
 this is test git hhhhh
 
 
+git init
+
+git config --global  user.name "flightxgf"
+git config --global  user.email "123.com"
+
 git add test.c
 
 git reset --hard HEAD^
+
+
+git checkout -- xxx
+
+git diff
+
+
 
